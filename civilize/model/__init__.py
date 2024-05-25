2939c01b287cbd5f6697712e028e3b6308c97940
Richard Bramley <rabramley@gmail.com>
2024-05-25 14:53:33 +0100

    Cases

--- civilize/model/__init__.py
+++ civilize/model/__init__.py
@@ -1,6 +1,7 @@
 from lbrc_flask.database import db
-from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import Boolean, Integer, String
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import Boolean, Integer, String, Date
+from datetime import date
 
 
 class CaseType(db.Model):
@@ -10,4 +11,22 @@ class CaseType(db.Model):
     name: Mapped[str] = mapped_column(String(64), nullable=False)
     title: Mapped[str] = mapped_column(String(64), nullable=False)
     description: Mapped[str] = mapped_column(String(255), nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean, nullable=True)
+    is_active: Mapped[bool] = mapped_column(Boolean, nullable=False)
+
+
+class Case(db.Model):
+    __tablename__ = 'civicrm_case'
+
+    id: Mapped[int] = mapped_column(Integer, nullable=False, primary_key=True)
+    case_type_id: Mapped[int] = mapped_column(Integer, nullable=False)
+    subject: Mapped[str] = mapped_column(String(128), nullable=False)
+    start_date: Mapped[date] = mapped_column(Date, nullable=False)
+    end_date: Mapped[date] = mapped_column(Date, nullable=False)
+    status_id: Mapped[int] = mapped_column(Integer, nullable=False)
+    is_deleted: Mapped[bool] = mapped_column(Boolean, nullable=False)
+
+    case_type: Mapped[CaseType] = relationship(
+        CaseType,
+        foreign_keys=[case_type_id],
+        primaryjoin='CaseType.id == Case.case_type_id',
+    )
--- create_test_db.py
+++ create_test_db.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python3
 
+from random import randint
 from dotenv import load_dotenv
 from lbrc_flask.database import db
 from lbrc_flask.security import init_roles, init_users
+from lbrc_flask.security.model import Role, User, roles_users
+from sqlalchemy import text
+from faker import Faker
+fake = Faker()
+
+from civilize.model import Case, CaseType
 
 load_dotenv()
 
@@ -10,7 +17,48 @@ from civilize import create_app
 
 application = create_app()
 application.app_context().push()
-db.create_all()
+
+User.__table__.create(bind=db.session.get_bind())
+Role.__table__.create(bind=db.session.get_bind())
+roles_users.create(bind=db.session.get_bind())
+
 init_roles()
 init_users()
 
+with open('civi_tables.sql', 'r') as f:
+    for s in f.read().split(';'):
+        if s.strip():
+            db.session.execute(text(f'{s};'))
+
+case_types = [
+    CaseType(name='GENVASC', title='GENVASC', is_active=True),
+    CaseType(name='BioResource', title='BioResource', is_active=True),
+    CaseType(name='Dinosaur', title='Dinosaur', is_active=False),
+    CaseType(name='BRICCS', title='BRICCS', is_active=True),
+    CaseType(name='GUITAR', title='GUITAR', is_active=True),
+    CaseType(name='BREAK', title='BREAK', is_active=True),
+    CaseType(name='STEAM', title='STEAM', is_active=True),
+    CaseType(name='GOAT', title='GOAT', is_active=True),
+    CaseType(name='RIFF', title='RIFF', is_active=True),
+    CaseType(name='SMOKE', title='SMOKE', is_active=True),
+    CaseType(name='LOOSE', title='LOOSE', is_active=True),
+    CaseType(name='BRIGHTEN', title='BRIGHTEN', is_active=True),
+    CaseType(name='TOAST', title='TOAST', is_active=True),
+    CaseType(name='LAKER', title='LAKER', is_active=True),
+]
+
+db.session.add_all(case_types)
+db.session.flush()
+
+cases = []
+
+for ct in case_types:
+    for _ in range(randint(50,100)):
+        cases.append(
+            Case(case_type_id=ct.id, subject=fake.sentence(), start_date=fake.date(), end_date=fake.date(), status_id=1, is_deleted=False)
+        )
+
+db.session.add_all(cases)
+db.session.flush()
+
+db.session.commit()
